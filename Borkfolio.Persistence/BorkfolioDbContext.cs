157a1196d154d16400d4d966807821d3057830ce
borkmeister <borkmaster12@gmail.com>
2024-04-10 22:42:27 -0500

    Add persistence project
    - complete initial db migration
    - add db context
    - add repositories

---
+++ Borkfolio.Persistence/BorkfolioDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Borkfolio.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Borkfolio.Persistence
+{
+    public class BorkfolioDbContext : DbContext
+    {
+        public BorkfolioDbContext(DbContextOptions<BorkfolioDbContext> options)
+            : base(options) { }
+
+        public DbSet<BoardGame> BoardGames { get; set; }
+        public DbSet<Suggestion> Suggestions { get; set; }
+
+    }
+}
---
+++ Borkfolio.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Borkfolio.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Borkfolio.Persistence.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly BorkfolioDbContext dbContext;
+
+        public BaseRepository(BorkfolioDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public virtual async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            dbContext.Set<T>().Update(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
