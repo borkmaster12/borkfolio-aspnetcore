28ddbbd0ee94882cb2f7d60cdb4e3e3a13402fda
borkmeister <borkmaster12@gmail.com>
2024-04-16 12:09:16 -0500

    Swap to Razor Pages frontend

--- Borkfolio.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Borkfolio.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
